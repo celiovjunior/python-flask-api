2e2dca5b851d48df56dc770fe059c7e465f85f43
Celio Junior <celio_cjr@hotmail.com>
2022-09-22 12:51:50 -0300

    fix the internal server error 500 about the class PeopleList

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
 from pydantic import BaseModel
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec("flask", title="Python API")
@@ -10,17 +11,28 @@ database = TinyDB('database.json')
 
 
 class People(BaseModel):
-    id: int
+    id: Optional[int]
     name: str
     gender: str
     address: str
     phone: str
 
 
+class PeopleList(BaseModel):
+    people_list: list[People]
+    count: int
+
+
 @server.get('/people')
+@spec.validate(resp=Response(HTTP_200=PeopleList))
 # @spec.validate(resp=Response(HTTP_200=People))
 def get_people():
-    return jsonify(database.all())
+    return jsonify(
+        PeopleList(
+            people_list=database.all(),
+            count=len(database.all())
+        ).dict()
+    )
 
 
 @server.post('/people')
@@ -29,6 +41,17 @@ def create_new_person():
     body = request.context.body.dict()
     database.insert(body)
     return body
+
+
+@server.put('/people/<int:id>')
+@spec.validate(
+    body=Request(People), resp=Response(HTTP_200=People)
+)
+def edit_people(id):
+    People = Query()
+    body = request.context.body.dict()
+    database.update(body, People.id == id)
+    return jsonify(body)
 
 
 server.run()
