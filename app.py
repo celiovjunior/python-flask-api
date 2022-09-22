24a8714a9850a75ecbb4a4a0dd3f7aa131265f46
Celio Junior <celio_cjr@hotmail.com>
2022-09-22 14:08:59 -0300

    made the delete method and used lambda next to auto the id

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+from itertools import count
 from typing import Optional
 from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from tinydb import TinyDB, Query
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec("flask", title="Python API")
 spec.register(server)
 database = TinyDB('database.json')
+c = count()
 
 
 class People(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = Field(default_factory=lambda: next(c))
     name: str
     gender: str
     address: str
@@ -27,12 +29,7 @@ class PeopleList(BaseModel):
 @spec.validate(resp=Response(HTTP_200=PeopleList))
 # @spec.validate(resp=Response(HTTP_200=People))
 def get_people():
-    return jsonify(
-        PeopleList(
-            people_list=database.all(),
-            count=len(database.all())
-        ).dict()
-    )
+    return jsonify(PeopleList(people_list=database.all(), count=len(database.all())).dict())
 
 
 @server.post('/people')
@@ -44,14 +41,19 @@ def create_new_person():
 
 
 @server.put('/people/<int:id>')
-@spec.validate(
-    body=Request(People), resp=Response(HTTP_200=People)
-)
+@spec.validate(body=Request(People), resp=Response(HTTP_200=People))
 def edit_people(id):
     People = Query()
     body = request.context.body.dict()
     database.update(body, People.id == id)
     return jsonify(body)
+
+
+@server.delete('/people/<int:id>')
+@spec.validate(resp=Response('HTTP_204'))
+def delete_people(id):
+    database.remove(Query().id == id)
+    return jsonify({})
 
 
 server.run()
